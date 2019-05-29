e96df55a33a3ea3d366e93728ea8d64400c14b80
Nabajit Das <nabajit.das@eqtribe.com>
2019-05-30 00:23:51 +0530

    user transaction function

--- utils/PayLater.js
+++ utils/PayLater.js
@@ -2,7 +2,9 @@ var users = [];
 var merchants = [];
 var interestRate = 0;
 var allTransactions = [];
-var UserCreditLimit = [];
+var userCreditLimit = [];
+var discountsByMerchant = [];
+
 
 module.exports = {
     addUser: function (userName, emailId, creditLimit) {
@@ -73,4 +75,86 @@ module.exports = {
         interestRate = rateOfInterest;
         return interestRate;
     },
+
+    userTxn: function (userName, merchantName, amount) {
+        var indexOfUser = users.findIndex(function (user) {
+            return user.userName === userName;
+        });
+
+        var indexOfMerchant = merchants.findIndex(function (merchant) {
+            return merchant.merchantName === merchantName;
+        });
+
+        if (indexOfMerchant === -1) {
+            return {
+                status: false,
+                msg: 'rejected!',
+                reason: 'mercent doesnot exist'
+            }
+        }
+        if (indexOfUser === -1) {
+            return {
+                status: false,
+                msg: 'rejected!',
+                reason: 'user doesnot exist'
+            }
+        }
+
+        if (amount > users[indexOfUser].creditLimit) {
+            //When the transaction amount is greater then credit limt of the user Reject transaction
+
+            allTransactions.push({
+                id: allTransactions.length+1,
+                fromUser: userName,
+                toMerchant: merchantName,
+                txnAmount: amount,
+                txnStatus: 'rejected',
+                remarks: 'credit limit'
+            });
+
+            return { status: false, msg: 'rejected!', reason: 'credit limit' };
+        }
+        else {
+            //When the transaction amount is less then or equal to credit limt of the user approve transaction
+            //Update credit limit of user
+            //if credit limit becomes zero, add user to UserCreditLimit
+            //add transaction to allTransactions
+            //update discount of marchant
+            var newCreditLimit = users[indexOfUser].creditLimit - amount;
+            users[indexOfUser].creditLimit = newCreditLimit;
+
+            if (newCreditLimit === 0) {
+                userCreditLimit.push(userName);
+            }
+
+            allTransactions.push({
+                id: allTransactions.length+1,
+                fromUser: userName,
+                toMerchant: merchantName,
+                txnAmount: amount,
+                txnStatus: 'sucess',
+                remarks: ''
+            });
+            
+            var indexOfMerchantDiscount = discountsByMerchant.findIndex(function (element) {
+                return element.merchantName === merchantName;
+            });
+            
+            if(indexOfMerchantDiscount === -1){
+                discountsByMerchant.push({
+                    merchant : merchantName,
+                    discount: _calcDiscountOfMerchant(amount, merchants[indexOfMerchant].discount)
+                });
+            }
+            else{
+                discountsByMerchant[indexOfMerchantDiscount].discount = _calcDiscountOfMerchant(amount, merchants[indexOfMerchant].discount)
+            }
+            return { status: true, msg: 'success!' }
+        }
+    }
+}
+
+
+function _calcDiscountOfMerchant(amount, discount){
+    return amount - (amount - (amount*discount/100));
 }
