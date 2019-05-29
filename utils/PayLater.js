9cdf68df7fcebfe11b40758c20bc687feb506ce8
Nabajit Das <nabajit.das@eqtribe.com>
2019-05-29 22:22:24 +0530

    add merchant function

--- utils/PayLater.js
+++ utils/PayLater.js
@@ -35,5 +35,37 @@ module.exports = {
             };
             return returnObj;
         }
+    },
+
+    addMerchant: function (merchantName, merchantEmail, discount) {
+        var returnObj = {};
+
+        var foundMerchant = merchants.find(function (merchant) {
+            return merchant.merchantName === merchantName;
+        });
+
+        if (foundMerchant === undefined) {
+            merchants.push({
+                merchantName: merchantName,
+                emailId: merchantEmail,
+                discount: discount
+            });
+
+            returnObj = {
+                status: true,
+                msg: 'Merchant created',
+                merchant: {
+                    merchantName: merchantName,
+                    discount: discount
+                }
+            }
+            return returnObj;
+        } else {
+            returnObj = {
+                status: false,
+                msg: 'Merchant already'
+            };
+            return returnObj;
+        }
     }
 }
